5becc1f04564392ce757ac8c90934ebd28c33f36
AiyionPrime <Aiyion.Prime@mail.com>
2014-07-14 20:06:03 +0200

    1.1

--- ExpCalc.cpp
+++ ExpCalc.cpp
@@ -7,8 +7,8 @@
 #include <conio.h>
 
 // author: Aiyion Prime
-// lastedit: 14.02.12 16:42
-// v1.0
+// lastedit: 20.02.12 23:14
+// v1.1
 
 
 using namespace std;
@@ -36,7 +36,7 @@ void startscreen()
     cout << "PrimeSoft presents:";
     wait(1);
     gotoxy(34, 5);
-    cout << "ExpCalc v1.0";
+    cout << "ExpCalc v1.1";
     wait(1);
     gotoxy(30, 7);
     cout << "Aiyion Prime - 2012- C";
@@ -79,12 +79,20 @@ int main(int argc, char *argv[])
     int goal;
     int exp;
     int getchvar;
+    int err=0; // Error Variable für jeden Fehler, der Durch die Eingabe von unmöglichen Leveln entsteht, wir err=err+1
     
     
     startscreen();
     
     while(1)
     {
+    err=0;
+    lvl=0;
+    goal=0;
+    exp=0;
+    getchvar=0;
+    
+    
     info();
       
     cout << "Aktueller Level:";
@@ -99,27 +107,62 @@ int main(int argc, char *argv[])
         cout << "Fehler: Ihr aktueller Level ist groesser als ihr Ziel." << endl;
         system("Pause");
         system("cls");
+        err=err+1;
     }
     if(goal==lvl)
     {
-        cout << "Fehler: Ihr aktueller Level identisch mit ihrem Ziel." << endl;
+        cout << "Fehler: Ihr aktueller Level ist identisch mit ihrem Ziel." << endl;
         system("Pause");
         system("cls");
+        err=err+1;
     }
     if(goal<0)
     {
         cout << "Fehler: Ihr Ziel ist ein negativer Level." << endl;
         system("Pause");
         system("cls");
+        err=err+1;
     }
     if(lvl<0)
     {
         cout << "Fehler: Ihr aktueller Level ist negativ." << endl;
         system("Pause");
         system("cls");
+        err=err+1;
     }
+    if(lvl>=200)
+    {
+        cout << "Warnung: Sie sollten Ihre Erfahrungslevel erst aufbrauchen, bevor sie weitere aufbauen." << endl;
+        system("Pause");
+        system("cls");
+    }
+    if (goal>=200)
+    {
+        cout << "Warnung: Es gibt keine Verzauberung, die eine solch hohe Zielstufe erfordert." << endl << "Sie sollten sich ein niedrigeres Ziel setzen." << endl;
+        system("pause");
+        system("cls");
+    }
+    if (lvl>10000 || goal > 10000)
+    {
+        cout << "Fehler: Mindestens einer der beiden angegebenen Level liegt ueber 10000." << endl << "Einen solch hohen Level zu erreichen ist nahezu unmoeglich." << endl << "Eine Berechnung groesserer Zahlen wuerde erheblich mehr Zeit erfordern" << endl << "Anfrage wird abgebrochen."<< endl;
+        system("pause");
+        system("cls");
+        err=err+1;
+    }
+    
+    
+    if (err==0)
+    {
     exp=experiencecalc(lvl, goal);
     cout << "Vom Level " << lvl << " bis zum Level " << goal << " werden " << exp << " Erfahrungspunkte benoetigt." << endl;
+    
+    }
+    if (err>0)
+    {
+        cout << "Es wurde(n) bei der Eingabe der Level " << err << " Fehler gemacht." << endl << "Geben sie reelle und realistische Werte ein." << endl << endl;
+        
+        
+    }
     cout << endl << "Zur Neuberechnung r(estart) druecken. Jede andere Taste beendet das Programm." << endl;
     
     getchvar = getch();
